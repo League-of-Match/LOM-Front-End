9bb7f7c077a35541a903812ebd3248f743453a01
Bernardo Guimaraes <110126139+megaurso@users.noreply.github.com>
2023-01-05 15:54:05 -0300

    Revert "Add register functionality"

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import { UserProvider } from "contexts/UserContext";
 import { MainRoutes as Routes } from "routes/MainRoutes";
 
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { Register } from "pages/Register/Register";
-
 export function App() {
   return (
     <UserProvider>
-      <Register></Register>
+      <Routes/>
     </UserProvider>
-  );
+  )
 }
